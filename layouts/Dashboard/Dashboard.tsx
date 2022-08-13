758aa47806ba1a3c066f6f27335674120bc337a5
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-13 18:13:04 -0400

    Create dashboard layout

---
+++ layouts/Dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import styles from './Dashboard.module.css';
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <section className={styles.dashboard}>
+            <h1>Dashboard</h1>
+        </section>
+    );
+};
+
+export default Dashboard;
