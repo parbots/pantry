d47b4a41fe1bb454ee13daa1a9aa74e57847a130
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-13 17:56:54 -0400

    Add navigation links to landing page header

--- layouts/pages/landing/Header/Header.tsx
+++ layouts/pages/landing/Header/Header.tsx
@@ -1,12 +1,30 @@
 import styles from './Header.module.css';
 
+import Link from 'next/link';
+
 import ThemeToggle from 'components/ThemeToggle';
+
+// TODO: Check login status and change login link to dashboard link if user is logged in
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h2 className={styles.title}>Pantry</h2>
-            <ThemeToggle />
+            <section className={styles.headerLeft}>
+                <h2 className={styles.title}>Pantry</h2>
+            </section>
+
+            <section className={styles.headerRight}>
+                <nav className={styles.navBar}>
+                    <ul className={styles.navList}>
+                        <li className={styles.navItem}>
+                            <Link href='/dashboard'>
+                                <a className={styles.navLink}>Login</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <ThemeToggle />
+            </section>
         </header>
     );
 };
