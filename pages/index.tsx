a349851802b83ebd1ee65bd98a521a08e6e77abe
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-12 21:39:40 -0400

    Configure initial template boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,21 @@ import styles from 'styles/HomePage.module.css';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+// TODO: Create favicon
+
 const HomePage: NextPage = () => {
     return (
         <div className={styles.page}>
             <Head>
                 <title>Next Starter</title>
-                <meta name='description' content='Next.js starter template' />
+                <meta
+                    name='description'
+                    content='All your developer ingredients, in one place!'
+                />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
-            <h1>Next.js Starter Template</h1>
+            <h1>Pantry</h1>
         </div>
     );
 };
