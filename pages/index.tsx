b591d9d0bde5ddfc48ae31f69172723ee200c024
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-13 15:26:26 -0400

    Implement landing page Header and Footer layouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import styles from 'styles/HomePage.module.css';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import Header from 'layouts/pages/landing/Header';
+import Footer from 'layouts/pages/landing/Footer';
+
 // TODO: Create favicon
 
 const HomePage: NextPage = () => {
@@ -17,17 +20,13 @@ const HomePage: NextPage = () => {
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
-            <header className={styles.header}>
-                <h1>Pantry</h1>
-            </header>
+            <Header />
 
             <main className={styles.main}>
                 <h2>All your developer ingredients, in one place.</h2>
             </main>
 
-            <footer className={styles.footer}>
-                <p className={styles.copyright}>© Parker Botsford 2022</p>
-            </footer>
+            <Footer />
         </div>
     );
 };
