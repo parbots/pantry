47d1caa53bb4740d63be70f247d5daa111187487
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-13 18:05:16 -0400

    Add link to homepage from dashboard page

--- layouts/pages/dashboard/Header/Header.tsx
+++ layouts/pages/dashboard/Header/Header.tsx
@@ -1,11 +1,17 @@
 import styles from './Header.module.css';
 
+import Link from 'next/link';
+
 import ThemeToggle from 'components/ThemeToggle';
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h2 className={styles.title}>Pantry</h2>
+            <h2 className={styles.title}>
+                <Link href='/'>
+                    <a className={styles.titleLink}>Pantry</a>
+                </Link>
+            </h2>
             <ThemeToggle />
         </header>
     );
