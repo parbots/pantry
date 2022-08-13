a08e6c3a608a515d281d6aef9fcb8e1b60d783df
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-13 15:26:05 -0400

    Create Footer landing page layout

---
+++ layouts/pages/landing/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p className={styles.copyright}>© Parker Botsford 2022</p>
+        </footer>
+    );
+};
+
+export default Footer;
