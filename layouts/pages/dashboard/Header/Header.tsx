a9c6852d24daaf973eb05b844b34b3ed3fa308a6
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-14 15:30:08 -0400

    Add navigation bar to dashboard header

--- layouts/pages/dashboard/Header/Header.tsx
+++ layouts/pages/dashboard/Header/Header.tsx
@@ -7,12 +7,25 @@ import ThemeToggle from 'components/ThemeToggle';
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h2 className={styles.title}>
-                <Link href='/'>
-                    <a className={styles.titleLink}>Pantry</a>
-                </Link>
-            </h2>
-            <ThemeToggle />
+            <section className={styles.headerTop}>
+                <h2 className={styles.title}>
+                    <Link href='/'>
+                        <a className={styles.titleLink}>Pantry</a>
+                    </Link>
+                </h2>
+                <ThemeToggle />
+            </section>
+            <section className={styles.headerBottom}>
+                <nav className={styles.navbar}>
+                    <ul className={styles.navList}>
+                        <li className={styles.navItem}>
+                            <Link href='/dashboard'>
+                                <a className={styles.navLink}>Dashboard</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
         </header>
     );
 };
