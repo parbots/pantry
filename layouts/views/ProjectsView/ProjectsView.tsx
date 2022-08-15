ef286434f1ad9b57fe9ef8e8ab7b4fcfd7967797
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-15 16:28:14 -0400

    Rename Projects Component to ProjectsView

---
+++ layouts/views/ProjectsView/ProjectsView.tsx
@@ -0,0 +1,14 @@
+
+import styles from './ProjectsView.module.css'
+import React from 'react';
+
+const ProjectsView = () => {
+    return (
+        <div>
+            <h1>ProjectsView</h1>
+        </div>
+    );
+};
+
+export default ProjectsView;
+
