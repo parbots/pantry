890ff7b5283736af4ed1cfadb88d0c411a205aad
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-15 16:26:47 -0400

    Create Projects view

---
+++ layouts/views/Projects/Projects.tsx
@@ -0,0 +1,14 @@
+
+import styles from './Projects.module.css'
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>Projects</h1>
+        </div>
+    );
+};
+
+export default Projects;
+
