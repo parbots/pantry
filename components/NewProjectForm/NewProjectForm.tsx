6108f396f1ac1855f09478abdb22a9e19c72df8a
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-16 17:21:05 -0400

    Create new project form component

---
+++ components/NewProjectForm/NewProjectForm.tsx
@@ -0,0 +1,14 @@
+
+import styles from './NewProjectForm.module.css'
+import React from 'react';
+
+const NewProjectForm = () => {
+    return (
+        <div>
+            <h1>NewProjectForm</h1>
+        </div>
+    );
+};
+
+export default NewProjectForm;
+
